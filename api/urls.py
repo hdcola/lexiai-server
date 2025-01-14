5dbb48fa2693ca7d2ea0621905fc902a5eeaf4db
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-14 15:29:56 -0500

    deleted url to text_to_text

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from .ai import text_to_text, text_to_speech
 
 urlpatterns = [
     path('users/register', register_user, name='register_user'),
-    path('text', text_to_text, name='text_to_text'),
-    path('speech', text_to_speech, name='text_to_speech'),
     path('languages', get_all_languages, name='get_all_languages'),
     path('topics', get_topics_for_level, name='get_topics_for_level'),
     path('styles', get_all_ai_styles, name='get_all_ai_styles')
