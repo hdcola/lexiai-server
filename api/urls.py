87736e52e1cbd04e6e938f7452cfe99f02a76361
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-14 15:30:40 -0500

    removed import .ai

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import register_user, get_all_languages, get_topics_for_level, get_all_ai_styles
-from .ai import text_to_text, text_to_speech
 
 urlpatterns = [
     path('users/register', register_user, name='register_user'),
