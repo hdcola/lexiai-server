cf376fc44fdc60e3ad3f5f418a840f3e338bf111
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-16 18:27:03 -0500

    login/register with password encryption (checking if email exists)

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_user, get_all_languages, get_topics_for_level, get_all_ai_styles
+from .views import register_user, login_user, get_all_languages, get_topics_for_level, get_all_ai_styles
 
 urlpatterns = [
     path('users/register', register_user, name='register_user'),
+    path('users/login', login_user, name='login_user'),
     path('languages', get_all_languages, name='get_all_languages'),
     path('topics', get_topics_for_level, name='get_topics_for_level'),
     path('styles', get_all_ai_styles, name='get_all_ai_styles')
--- api/views.py
+++ api/views.py
@@ -6,9 +6,12 @@ import datetime
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from pymongo import MongoClient
+import bcrypt
+import jwt 
 
 mongo_uri = os.getenv('MONGO_URI')
 db_name = os.getenv('DB_NAME')
+token_key = os.getenv('TOKEN_KEY')
 
 client = MongoClient(mongo_uri)
 db = client[db_name]
@@ -19,20 +22,75 @@ styles_collection = db['styles']
 
 logging.basicConfig(level=logging.INFO)
 
-# users
+# user register
 @api_view(['POST'])
 def register_user(request):
-    data = request.data
+    try:
+        data = request.data
+        # Find user by email
+        user = users_collection.find_one({ "email" : data['email']}) 
+        if user:
+            return Response({"message": "Email already exists."}, status=400)
 
-    user_data = {
-        'username': data['username'],
-        'email': data['email'],
-        'password': data['password'],
-        'createdAt': datetime.datetime.now(), 
-    }
+        # Hash the password
+        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+
+        user_data = {
+            'username': data['username'],
+            'email': data['email'],
+            'password': hashed_password.decode('utf-8'),
+            'createdAt': datetime.datetime.now(), 
+        }
+        
+        users_collection.insert_one(user_data)
+        return Response({'message': 'User registered successfully'})
+    except Exception as e:
+        logging.exception("Error occurred during registration: %s", e)
+        return Response({"error": "Error during the registration"}, status=500)
+
+# user log in
+@api_view(['POST'])
+def login_user(request):
+    try:
+        data = request.data
+
+        # check if email and password are present
+        if not data.get('email') or not data.get('password'):
+            return Response({"message": "Email and password are required."}, status=400)
     
-    users_collection.insert_one(user_data)
-    return Response({'message': 'User registered successfully'})
+        # Find user by email
+        logged_user  = users_collection.find_one({ "email" : data['email']})        
+        if logged_user:
+             # verify the password
+            if bcrypt.checkpw(data['password'].encode('utf-8'), logged_user['password'].encode('utf-8')):
+                # convert ObjectId to string
+                logged_user['_id'] = str(logged_user['_id'])
+                del logged_user['password']  # remove password from the response
+
+                payload = {
+                    'email': logged_user['email'],
+                    'id': str(logged_user['_id'])  # convert ObjectId to string
+                    # add expiration
+                }
+                access_token = jwt.encode(payload, token_key, algorithm='HS256')
+
+                return Response({
+                    "message": "Login successful",
+                    "user": logged_user,
+                    "accessToken": access_token,
+                }, status=200)
+            else:
+                return Response({"message": "Invalid credentials"}, status=401)
+        else:
+            return Response({
+                "message": "Invalid credentials"
+            }, status=401)
+
+    except Exception as e:
+        logging.exception("Error occurred during login: %s", e)
+        return Response({"error": "Error logging in"}, status=500)
+    
+
 
 # Languages
 @api_view(['GET'])
@@ -68,6 +126,6 @@ def get_topics_for_level(request):
         logging.exception("Error fetching topics for language and level: %s", e)
         return Response({"message": "An error occurred while fetching topics."}, status=500)
 
-
+# helper function to parse JSON
 def parse_json(data):
     return json.loads(json_util.dumps(data))
