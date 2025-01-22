f9388ce0c541c2df6529f729a1d2dc957fb5c488
hdcola <gh@hdcola.org>
2025-01-22 11:12:02 -0400

    don't need User

--- api/settings/views.py
+++ api/settings/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from ..models import User
 from ..authentication import JWTAuthentication
 from pymongo import MongoClient
 from bson import ObjectId
