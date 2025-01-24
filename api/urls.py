99554b8b6e5f8ea4902361def7186ab959254642
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-24 03:16:45 -0500

    save security settings

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from .settings.views import SettingsView
 from .favorites.views import FavoritesView
-from .users.views import register_user, login_user, update_user_profile
+from .users.views import register_user, login_user, update_user_profile, change_user_password
 from .views import get_all_languages, get_language_by_id, get_topics_for_level, get_all_ai_styles, get_ai_style_by_id, validate_jwt
 
 urlpatterns = [
     path('users/register', register_user, name='register_user'),
     path('users/login', login_user, name='login_user'),
     path('users/update', update_user_profile, name='update_user_profile'),
+    path('users/security', change_user_password, name='change_user_password'),
     path('jwt/validate', validate_jwt, name='validate_jwt'),
     path('languages', get_all_languages, name='get_all_languages'),
     path('languages/<str:id>', get_language_by_id, name='get_language_by_id'),
